f77a0be8d8bf0ecc55f136858cbaf0bbb5c6237f
Linus Johansson <golfarenlj@gmail.com>
2025-01-23 22:24:00 +0000

    Enhance ProfileSerializer and views to include follower and following counts

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 
+from followers.models import Follower
+
 from .models import Profile
 
 MAX_BIO_LENGTH = 500
@@ -12,22 +14,31 @@ class ProfileSerializer(serializers.ModelSerializer):
     profile_picture_url = serializers.ReadOnlyField(
         source='profile_picture.url'
     )
+    followers_count = serializers.ReadOnlyField()
+    following_count = serializers.ReadOnlyField()
+    is_following = serializers.SerializerMethodField()
 
-    class Meta:
-        model = Profile
-        fields = [
-            'id',
-            'owner',
-            'name',
-            'profile_picture_url',
-            'profile_picture',
-            'bio',
-            'website',
-            'post_count',
-            'created_at',
-            'updated_at',
-        ]
-        read_only_fields = ['id', 'owner', 'created_at', 'updated_at']
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
+
+    def get_following_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            following = Follower.objects.filter(
+                owner=user,
+                followed=obj.owner,
+            ).first()
+            return following.id if following else None
+        return None
+
+    def get_is_following(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            return Follower.objects.filter(
+                owner=user, followed=obj.owner
+            ).exists()
+        return False
 
     def validate_website(self, value):
         if value and not value.startswith('http'):
@@ -40,3 +51,23 @@ class ProfileSerializer(serializers.ModelSerializer):
             msg = f'Bio must be {MAX_BIO_LENGTH} characters or less.'
             raise serializers.ValidationError(msg)
         return value
+
+    class Meta:
+        model = Profile
+        fields = [
+            'id',
+            'owner',
+            'name',
+            'profile_picture_url',
+            'profile_picture',
+            'bio',
+            'website',
+            'post_count',
+            'following_id',
+            'followers_count',
+            'following_count',
+            'is_following',
+            'created_at',
+            'updated_at',
+        ]
+        read_only_fields = ['id', 'owner', 'created_at', 'updated_at']
--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,6 @@
-from rest_framework import generics
+from django.db.models import Count
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters, generics
 
 from blogit.permissions import IsOwnerOrReadOnly
 
@@ -9,13 +11,41 @@ from .serializers import ProfileSerializer
 class ProfileList(generics.ListAPIView):
     """List all profiles."""
 
-    queryset = Profile.objects.order_by('-created_at')
+    queryset = (
+        Profile.objects.annotate(
+            posts_count=Count('owner__posts', distinct=True),
+            followers_count=Count('owner__followed', distinct=True),
+            following_count=Count('owner__following', distinct=True),
+        )
+        .select_related('owner')
+        .order_by('-created_at')
+    )
     serializer_class = ProfileSerializer
+    filter_backends = [filters.OrderingFilter, DjangoFilterBackend]
+    filterset_fields = [
+        'owner__following__followed__profile',
+        'owner__followed__owner__profile',
+    ]
+    ordering_fields = [
+        'posts_count',
+        'followers_count',
+        'following_count',
+        'owner__following__created_at',
+        'owner__followers__created_at',
+    ]
 
 
 class ProfileDetail(generics.RetrieveUpdateDestroyAPIView):
     """Retrieve, update, or delete a profile."""
 
     permission_classes = [IsOwnerOrReadOnly]
-    queryset = Profile.objects.order_by('-created_at')
+    queryset = (
+        Profile.objects.annotate(
+            posts_count=Count('owner__posts', distinct=True),
+            followers_count=Count('owner__followed', distinct=True),
+            following_count=Count('owner__following', distinct=True),
+        )
+        .select_related('owner')
+        .order_by('-created_at')
+    )
     serializer_class = ProfileSerializer
