6bfe0b0402ac08c31e912b802474d8ccab48bf30
Linus Johansson <golfarenlj@gmail.com>
2025-01-23 18:22:41 +0000

    Refactor ProfileSerializer to simplify fields and enhance readability

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -4,20 +4,7 @@ from .models import Profile
 
 
 class ProfileSerializer(serializers.ModelSerializer):
-    """Serializer for the Profile model.
-    This serializer handles creating and updating of profiles.
-    """
-
-    owner = serializers.ReadOnlyField(source='owner.username')
-    is_owner = serializers.SerializerMethodField()
-    following_id = serializers.SerializerMethodField()
-    posts_count = serializers.ReadOnlyField()
-    followers_count = serializers.ReadOnlyField()
-    following_count = serializers.ReadOnlyField()
-
-    def get_is_owner(self, obj):
-        request = self.context['request']
-        return request.user == obj.owner
+    """Serializer for the Profile model."""
 
     class Meta:
         model = Profile
@@ -29,5 +16,5 @@ class ProfileSerializer(serializers.ModelSerializer):
             'website',
             'created_at',
             'updated_at',
-            'is_owner',
         ]
+        read_only_fields = ['id', 'owner', 'created_at', 'updated_at']
