9e5fc887ad3d2e4674a952c79634ff5c6eea9985
Linus Johansson <golfarenlj@gmail.com>
2025-01-23 18:22:18 +0000

    Simplify root_route response and remove unnecessary status handling

--- blogit/views.py
+++ blogit/views.py
@@ -1,15 +1,7 @@
-from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 
-@api_view(['GET'])
-def root_route(_):
-    return Response(
-        {
-            'message': 'Welcome to the Blogit API.',
-            'version': '1.0.0',
-            'status': 'OK',
-        },
-        status=status.HTTP_200_OK,
-    )
+@api_view()
+def root_route(request):
+    return Response({'message': 'Welcome to Blogit API!'})
