221be72f7599f2a0dec50d70a70c7e0e4a030ab9
Linus Johansson <golfarenlj@gmail.com>
2025-01-23 18:22:32 +0000

    Simplify ProfileAdmin registration in admin panel

--- profiles/admin.py
+++ profiles/admin.py
@@ -2,14 +2,4 @@ from django.contrib import admin
 
 from .models import Profile
 
-
-@admin.register(Profile)
-class ProfileAdmin(admin.ModelAdmin):
-    list_display = [
-        'owner',
-        'name',
-        'website',
-        'created_at',
-        'updated_at',
-    ]
-    search_fields = ['owner__username', 'name']
+admin.site.register(Profile)
