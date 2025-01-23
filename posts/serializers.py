aa20e1134533da9929f47e58b4de5df89219eb1a
Linus Johansson <golfarenlj@gmail.com>
2025-01-23 23:53:22 +0000

    Enhance PostSerializer with like functionality and validation improvements

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,6 +1,8 @@
 from django.utils.timesince import timesince
 from rest_framework import serializers
 
+from likes.models import Like
+
 from .models import Post
 
 
@@ -8,6 +10,34 @@ class PostSerializer(serializers.ModelSerializer):
     """Serializer for Post model."""
 
     human_readable_created_at = serializers.SerializerMethodField()
+    like_id = serializers.SerializerMethodField()
+    likes_count = serializers.SerializerMethodField()
+    user_has_liked = serializers.SerializerMethodField()
+
+    def get_human_readable_created_at(self, obj):
+        return timesince(obj.created_at)
+
+    def get_like_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            like = Like.objects.filter(owner=user, post=obj).first()
+            return like.id if like else None
+        return None
+
+    def get_likes_count(self, obj):
+        return obj.likes.count()
+
+    def get_user_has_liked(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            return Like.objects.filter(owner=user, post=obj).exists()
+        return False
+
+    def validate_content(self, value):
+        if not value and not self.initial_data.get('image'):
+            msg = 'You must upload an image or write some content.'
+            raise serializers.ValidationError(msg)
+        return value
 
     class Meta:
         model = Post
@@ -19,14 +49,8 @@ class PostSerializer(serializers.ModelSerializer):
             'created_at',
             'updated_at',
             'human_readable_created_at',
+            'like_id',
+            'likes_count',
+            'user_has_liked',
         ]
         read_only_fields = ['owner', 'created_at', 'updated_at']
-
-    def get_human_readable_created_at(self, obj):
-        return timesince(obj.created_at)
-
-    def validate_content(self, value):
-        if not value and not self.initial_data.get('image'):
-            msg = 'You must upload an image or write some content.'
-            raise serializers.ValidationError(msg)
-        return value
