d20b44be6df5ed4e94ba60c28d5a9472321cab77
Linus Johansson <golfarenlj@gmail.com>
2025-01-23 14:45:25 +0000

    Add ProfileList and ProfileDetail views for API endpoints

--- profiles/views.py
+++ profiles/views.py
@@ -1,1 +1,19 @@
-# Create your views here.
+from rest_framework import generics, permissions
+
+from .models import Profile
+from .serializers import ProfileSerializer
+
+
+class ProfileList(generics.ListCreateAPIView):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class ProfileDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
