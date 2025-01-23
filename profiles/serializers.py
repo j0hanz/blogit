e7eb0791716cd4ceb510e38e7ea49e4e1379212d
Linus Johansson <golfarenlj@gmail.com>
2025-01-23 18:41:44 +0000

    Add validation for website URL and bio length in ProfileSerializer

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -2,6 +2,8 @@ from rest_framework import serializers
 
 from .models import Profile
 
+MAX_BIO_LENGTH = 500
+
 
 class ProfileSerializer(serializers.ModelSerializer):
     """Serializer for the Profile model."""
@@ -18,3 +20,15 @@ class ProfileSerializer(serializers.ModelSerializer):
             'updated_at',
         ]
         read_only_fields = ['id', 'owner', 'created_at', 'updated_at']
+
+    def validate_website(self, value):
+        if value and not value.startswith('http'):
+            msg = 'Website URL must start with http or https.'
+            raise serializers.ValidationError(msg)
+        return value
+
+    def validate_bio(self, value):
+        if len(value) > MAX_BIO_LENGTH:
+            msg = f'Bio must be {MAX_BIO_LENGTH} characters or less.'
+            raise serializers.ValidationError(msg)
+        return value
