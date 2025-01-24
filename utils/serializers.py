65d770d10ff99e75ca1bff4041e5c527a7452692
Linus Johansson <golfarenlj@gmail.com>
2025-01-24 18:38:30 +0000

    Refactor BaseSerializer to inherit from OwnerInfoMixin for ownership and timestamp fields

--- utils/serializers.py
+++ utils/serializers.py
@@ -1,18 +1,7 @@
-from django.contrib.humanize.templatetags.humanize import naturaltime
 from rest_framework import serializers
 
+from utils.mixins import OwnerInfoMixin
 
-class BaseSerializer(serializers.ModelSerializer):
-    is_owner = serializers.SerializerMethodField()
-    created_at = serializers.SerializerMethodField()
-    updated_at = serializers.SerializerMethodField()
 
-    def get_is_owner(self, obj: object) -> bool:
-        request = self.context['request']
-        return request.user == obj.owner
-
-    def get_created_at(self, obj: object) -> str:
-        return naturaltime(obj.created_at)
-
-    def get_updated_at(self, obj: object) -> str:
-        return naturaltime(obj.updated_at)
+class BaseSerializer(OwnerInfoMixin, serializers.ModelSerializer):
+    pass
