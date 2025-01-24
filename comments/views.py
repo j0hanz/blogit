202461c6a65ef75bc730fba20a3383fbb021c302
Linus Johansson <golfarenlj@gmail.com>
2025-01-24 22:01:04 +0000

    Enhance CommentViewSet with error handling and logging mixins; optimize queryset to use select_related

--- comments/views.py
+++ comments/views.py
@@ -1,18 +1,23 @@
+import logging
+
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from blogit.permissions import IsOwnerOrReadOnly
+from utils.mixins import ErrorHandlingMixin, LoggingMixin
 from utils.pagination import StandardResultsSetPagination
 from utils.viewsets import BaseViewSet
 
 from .models import Comment
 from .serializers import CommentDetailSerializer, CommentSerializer
 
+logger = logging.getLogger(__name__)
 
-class CommentViewSet(BaseViewSet):
+
+class CommentViewSet(ErrorHandlingMixin, LoggingMixin, BaseViewSet):
     """ViewSet for Comment model."""
 
-    queryset = Comment.objects.all()
+    queryset = Comment.objects.select_related('owner', 'post').all()
     serializer_class = CommentSerializer
     permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
     filter_backends = [DjangoFilterBackend]
