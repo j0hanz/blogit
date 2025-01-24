70a825bb8e63197fcf987c015590fb521ba8b968
Linus Johansson <golfarenlj@gmail.com>
2025-01-24 12:19:58 +0000

    Refactor validate_content function to streamline error handling and improve readability

--- utils/validators.py
+++ utils/validators.py
@@ -7,12 +7,8 @@ def validate_content(value: str, image: str = None) -> str:
     """Validates the content length and presence of content or image."""
     if not value and not image:
         msg = 'You must upload an image or write some content.'
-        raise ValidationError(
-            msg
-        )
+        raise ValidationError(msg)
     if len(value) > MAX_CONTENT_LENGTH:
         msg = f'Content cannot exceed {MAX_CONTENT_LENGTH} characters.'
-        raise ValidationError(
-            msg
-        )
+        raise ValidationError(msg)
     return value
