82c8aa148427c9aa1fe225d0c8f8e75fef4dba9b
Linus Johansson <golfarenlj@gmail.com>
2025-01-25 23:40:37 +0000

    Add content validation function and remove old validator file

--- utils/validation.py
+++ utils/validation.py
@@ -1,4 +1,7 @@
+from django.core.exceptions import ValidationError
 from rest_framework import serializers
+
+MAX_CONTENT_LENGTH = 500
 
 
 def validate_actor_and_recipient(data):
@@ -12,4 +15,15 @@ def validate_followed_user(request_user, value):
     if request_user == value:
         msg = 'You cannot follow yourself.'
         raise serializers.ValidationError(msg)
+    return value
+
+
+def validate_content(value: str, image: str = None) -> str:
+    """Validates the content length and presence of content or image."""
+    if not value and not image:
+        msg = 'You must upload an image or write some content.'
+        raise ValidationError(msg)
+    if len(value) > MAX_CONTENT_LENGTH:
+        msg = f'Content cannot exceed {MAX_CONTENT_LENGTH} characters.'
+        raise ValidationError(msg)
     return value
